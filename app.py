662d407889f03e4215de30a0e79591029c73298c
WasilaRehman <105180490+WasilaRehman@users.noreply.github.com>
2023-03-12 04:03:16 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,44 +5,6 @@ import pickle
 import pandas as pd  # For data handling
 pd.set_option('display.max_colwidth', 1000)
 import numpy as np # linear algebra
-from time import time  # To time our operations
-from collections import defaultdict  # For word frequency
-#import spacy  # For preprocessing
-from spacy.lang.en import English
-#from spacy import displacy
-#import textacy
-from textacy import extract, text_stats
-from textblob import TextBlob
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.model_selection import GridSearchCV
-from sklearn.pipeline import Pipeline
-from sklearn.metrics.pairwise import cosine_similarity
-from collections import Counter, defaultdict
-from sklearn.preprocessing import LabelEncoder
-from operator import contains
-from sklearn.cluster import KMeans, AgglomerativeClustering, MiniBatchKMeans, MeanShift
-from sklearn.metrics.cluster import normalized_mutual_info_score, silhouette_score
-from scipy.cluster.hierarchy import dendrogram, linkage
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-from matplotlib import pyplot as plt
-from yellowbrick.cluster import KElbowVisualizer, SilhouetteVisualizer
-from sklearn.decomposition import TruncatedSVD
-
-#import nltk
-from nltk.tokenize import word_tokenize
-from nltk.stem import PorterStemmer
-
-from gensim.models import KeyedVectors
-import multiprocessing
-from gensim.models import Word2Vec
-import gensim 
-import logging
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-
-import logging  # Setting up the loggings to monitor gensim
-logging.basicConfig(format="%(levelname)s - %(asctime)s: %(message)s", datefmt= '%H:%M:%S', level=logging.INFO)
-
-stemmer = PorterStemmer()
 
 import pickle
 import streamlit as st
